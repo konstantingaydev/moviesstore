c1f6b3eaeebf5c0da359f40a129b0118b44abae7
konstantingaydev <konstantingaydev16@gmail.com>
2025-09-17 21:57:59 -0400

    added list of reviews

--- home/views.py
+++ home/views.py
@@ -1,12 +1,40 @@
 from django.shortcuts import render
-
+from movies.models import Review
 # Create your views here.
 
+# def home(request):
+#     # This logic now lives in your home app's view
+#     top_reviews = Review.objects.select_related('user', 'movie').order_by('-date')[:10]
 
-def index(request):
+#     # --- Add this line for debugging ---
+#     print(top_reviews) 
+#     # -----------------------------------
+
+#     context = {
+#         'top_reviews': top_reviews,
+#     }
+#     # Make sure it renders your index.html template
+#     return render(request, 'index.html', context)
+
+# def index(request):
+#     template_data = {}
+#     template_data['title'] = 'Movies Store'
+#     return render(request, 'home/index.html', {'template_data': template_data})
+def home(request):
+    # Data from your old 'index' view
     template_data = {}
     template_data['title'] = 'Movies Store'
-    return render(request, 'home/index.html', {'template_data': template_data})
+    
+    # Data for the reviews
+    top_reviews = Review.objects.select_related('user', 'movie').order_by('-date')[:10]
+    
+    # Combine everything into one context dictionary
+    context = {
+        'template_data': template_data,
+        'top_reviews': top_reviews,
+    }
+    
+    return render(request, 'home/index.html', context)
 
 def about(request):
     template_data = {}
